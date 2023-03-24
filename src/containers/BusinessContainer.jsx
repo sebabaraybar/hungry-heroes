567c692e8af3267c7e8130b20b46089430ce5d13
camilascalzo <camilascalzo@hotmail.com>
2023-03-23 21:42:59 -0300

    business general WIP

--- src/containers/BusinessContainer.jsx
+++ src/containers/BusinessContainer.jsx
@@ -1,32 +1,27 @@
 import React from 'react';
-import boxList from '../box.json';
-import { Box, Button } from '@mui/material';
-import BoxCard from '../components/ui/BoxCard/BoxCard';
+import businessList from '../business.json';
+import { Box } from '@mui/material';
+import BusinessCard from '../components/ui/BusinessCard/BusinessCard';
 import logo from '../media/logo.svg';
 import styles from './BusinessContainer.module.scss';
+import vars from '../styles/variables.scss';
 
 const BusinessContainer = function () {
 	return (
-		<Box className={styles.container}>
-			<Box className={styles.btnContainer}>
-				<Button
-					variant='contained'
-					color='secondary'
-				>
-						Crear Box
-				</Button>
-			</Box>
+		<Box className={styles.container} >
 			<Box className={styles.cardContainer}>
-				{boxList.map((box) => (
-					<BoxCard
-					title={box.name}
-					alt={`Logo de ${box.name}`}
-					//esto va a venir del back, ver cómo
+				{businessList.map((business) => (
+					<BusinessCard 
+					title={business.name}
+					subtitle={business.slogan}
+					alt={`Logo de ${business.name}`}
+					// esto va a venir del back, ver cómo
 					logo={logo}
-					//hay que definir cant de caracteres
-					description={box.detail}
+					// hay que definir cant de caracteres
+					description={business.description}
+					btnTitle="Comprar"
 					/>
-				))};
+				))}
 			</Box>
 		</Box>
 	);
