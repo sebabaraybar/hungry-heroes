d52e027ed1b9cc65ad012e10bfdf71510da5965b
camilascalzo <camilascalzo@hotmail.com>
2023-04-19 19:18:09 -0300

    header fix design

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import LargeDisplay from './LargeDisplay/LargeDisplay';
-import { Box, useMediaQuery, AppBar, Toolbar } from '@mui/material';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Box, AppBar, Toolbar, List, ListItemText, Menu } from '@mui/material';
+import CButton from '../../ui/Button/CButton';
+import ROUTES_ENUM from '../../../enums/routesEnum';
 import { getHome } from '../../../utils/navUtils';
 import logo from '../../../media/logo.png'
 import styles from './Header.module.scss';
+// import { ExitToAppRounded, LockRounded, ManageAccountsRounded } from '@mui/icons-material';
 
 const Header = function () {
 
-	const isMobile = useMediaQuery('(max-width:1024px)');
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+	const navigate = useNavigate();
+
+	const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+	const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+	const onLogout = () => {
+		// AuthService.logout();
+		navigate(ROUTES_ENUM.AUTH_LOGIN);
+	};
 
   return (
 		<AppBar
@@ -25,8 +42,70 @@ const Header = function () {
 					/>
 					</Link>
 				</Box>
-				<Box className={styles.actionsContainer}>
-					<LargeDisplay />
+				<Box className={styles.menu}>
+					<CButton
+						onClick={handleClick}
+						variant="outlined"
+						color="secondary"
+						title="menú"
+						sx={{fontSize: "2rem"}}
+					/>
+					<Menu
+						open={open}
+						onClose={handleClose}
+						anchorEl={anchorEl}
+						anchorOrigin={{
+							vertical: 'bottom',
+							horizontal: 'right'
+						}}
+						transformOrigin={{
+							horizontal: 'right',
+							vertical: 'top'
+						}}
+						classes={{ paper: styles['menu-paper'] }}
+					>
+				{/* {getItemsForRol().map((item) => (
+					<MenuItem
+						key={item.key}
+						className={location.pathname === item.link ? styles.navItemActive : styles.navItem }
+						onClick={() => navigate(item.link)}
+					>
+						<Typography>{item.label}</Typography>
+					</MenuItem>
+		 	))} */}
+						<List>
+							<ListItemText sx={{ textAlign: 'left'}} >
+								<CButton
+									variant="text"
+									color="secondary"
+									title="Perfil"
+									// startIcon={<ManageAccountsRounded />}
+									sx={{ fontSize: '1.3rem'}}
+									// onClick={}
+								/>
+							</ListItemText>
+							<ListItemText sx={{ textAlign: 'left'}}>
+								<CButton
+									variant="text"
+									color="secondary"
+									title="Cambiar contraseña"
+									sx={{ fontSize: '1.3rem'}}
+									// startIcon={ <LockRounded /> }
+									onClick={() => navigate(ROUTES_ENUM.AUTH_CHANGE_PASS)}
+								/>
+							</ListItemText>
+							<ListItemText sx={{ textAlign: 'left'}}>
+								<CButton
+									variant="text"
+									color="secondary"
+									title="Cerrar sesión"
+									sx={{ fontSize: '1.3rem'}}
+									// startIcon={ <ExitToAppRounded /> }
+									onClick={onLogout}
+								/>
+							</ListItemText>
+						</List>
+					</Menu>
 				</Box>
 			</Toolbar>
 		</AppBar>
